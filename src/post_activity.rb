277a329aadebd6509fc3cd928654183943ab713a
hilohiro <hilohiro+bitbucket@gmail.com>
2013-04-14 14:40:07 +0900

    Modified to show the posting ui.

--- src/post_activity.rb
+++ src/post_activity.rb
@@ -5,20 +5,27 @@ require 'ruboto/util/toast'
 ruboto_import_widgets :Button, :LinearLayout, :EditText, :TextView
 
 java_import 'android.util.Log'
+java_import 'android.text.InputType'
+java_import 'android.view.inputmethod.EditorInfo'
 
 # http://xkcd.com/378/
 
 class PostActivity
   def onCreate(bundle)
     super
-    set_title 'ここはタイトル'
+    set_title 'twitterに投稿'
 
     self.content_view =
         linear_layout :orientation => :vertical do
-          @text_view = text_view :text => 'What hath Matz wrought?', :id => 42, :width => :match_parent,
+          text_view :text => 'URL', :id => 45, :width => :match_parent
+          @url_edit = edit_text :id => 46, :width => :match_parent, :height => :wrap_content, :text => '',
+                                :input_type => InputType.TYPE_CLASS_TEXT | InputType::TYPE_TEXT_VARIATION_URI
+          text_view :text => 'メッセージ', :id => 47, :width => :match_parent
+          @edit_view = edit_text :id => 44, :width => :match_parent, :height => :wrap_content, :text => '',
+                                 :ime_options => EditorInfo::IME_ACTION_SEND
+          button :text => '投稿', :width => :match_parent, :id => 43, :on_click_listener => proc { apply_to_label }
+          @text_view = text_view :text => '', :id => 42, :width => :match_parent,
                                  :gravity => :center, :text_size => 48.0
-#          @edit_view = edit_text :id => 44, :width => :match_parent, :input_type => :text
-          button :text => 'M-x butterfly', :width => :match_parent, :id => 43, :on_click_listener => proc { butterfly }
         end
   rescue
     puts "Exception creating activity: #{$!}"
@@ -27,9 +34,8 @@ class PostActivity
 
   private
 
-  def butterfly
-#    toast @edit_view.text[0..10]
-    toast 'Flipped a bit via butterfly'
+  def apply_to_label
+    @text_view.text = @edit_view.text
   end
 
 end
